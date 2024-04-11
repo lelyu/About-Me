9d8404c0dc3d2e09287ff86b63afa44f00a4797c
Lok Leoi <lyulelok@gmail.com>
2024-04-11 17:12:38 -0400

    Updates

---
+++ precache-manifest.9d31f278351226bed1da4c2f4a234f60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0c3968dbe17c19273c53ed898f2400",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b1543a51f042c1f0735",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "b8f05c7dcbe05d4fecb7",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "9b1543a51f042c1f0735",
+    "url": "/static/js/2.b65a2958.chunk.js"
+  },
+  {
+    "revision": "60c46b200b0cfd41617f",
+    "url": "/static/js/3.b265e9c7.chunk.js"
+  },
+  {
+    "revision": "b8f05c7dcbe05d4fecb7",
+    "url": "/static/js/main.9e9d5bfd.chunk.js"
+  },
+  {
+    "revision": "16d55b58bd5e69b84081",
+    "url": "/static/js/runtime~main.b36d5e71.js"
+  }
+]);
